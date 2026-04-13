ceb27ead0fb4b57b90f4d11ea13d3f8dea817724
Justin <justin.uaisele@gmail.com>
2026-04-14 11:55:14 +1200

    feat: add student notes data array

---
+++ app/data.php
@@ -0,0 +1,36 @@
+<?php
+
+$studentNotes = [
+    [
+        "student_name" => "Andy",
+        "note_text" => "Needs help with arrays",
+        "status" => "open"
+    ],
+    [
+        "student_name" => "Mia",
+        "note_text" => "Completed loop practice",
+        "status" => "open"
+    ],
+    [
+        "student_name" => "Noah",
+        "note_text" => "Strong with functions",
+        "status" => "closed"
+    ],
+    [
+        "student_name" => "Ava",
+        "note_text" => "Needs more work on conditionals",
+        "status" => "closed"
+    ],
+    [
+        "student_name" => "Jay",
+        "note_text" => "Good browser output",
+        "status" => "open"
+    ],
+    [
+        "student_name" => "Lena",
+        "note_text" => "Practicing associative arrays",
+        "status" => "closed"
+    ],
+
+
+];
