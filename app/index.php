3eb7c21b09a8a27a0db8c199f2f999c14445cd5f
Justin <justin.uaisele@gmail.com>
2026-04-14 13:01:35 +1200

    feat: add all notes page

---
+++ app/index.php
@@ -0,0 +1,30 @@
+<?php
+require_once "data.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Student Notes</title>
+</head>
+
+<body>
+
+    <h1>Student Notes</h1>
+    <?php foreach ($studentNotes as $studentNote) { ?>
+        <h1>
+            <?php echo $studentNote["student_name"] ?>
+        </h1>
+        <p>
+            <?php echo $studentNote["note_text"] ?>
+        </p>
+        <p>
+            <?php echo $studentNote["status"] ?>
+        </p>
+    <?php } ?>
+
+</body>
+
+</html>
